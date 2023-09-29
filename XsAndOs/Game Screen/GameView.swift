d359810389855efe415871b1727012b12ff0d2d5
JanetV <victoriajanet051@gmail.com>
2023-09-29 14:57:39 -0400

    challenge device completed

--- XsAndOs/Game Screen/GameView.swift
+++ XsAndOs/Game Screen/GameView.swift
@@ -22,6 +22,11 @@ struct GameView: View {
                 .buttonStyle(PlayerButtonStyle(isCurrent: game.player1.isCurrent))
                 Button(game.player2.name){
                     game.player2.isCurrent = true
+                    if game.gameType == .bot{
+                        Task{
+                            await game.deviceMove()
+                        }
+                    }
                 }
                 .buttonStyle(PlayerButtonStyle(isCurrent: game.player2.isCurrent))
             }
@@ -44,6 +49,16 @@ struct GameView: View {
                         SquareView(index: index)
                     }
                     
+                }
+            }
+            .overlay{
+                if game.isThinking{
+                    VStack{
+                        Text(" Thinking... ")
+                            .foregroundColor(Color(.systemBackground))
+                            .background(Rectangle().fill(Color.primary))
+                    ProgressView()
+                    }
                 }
             }
             .disabled(game.boardDisabled)
